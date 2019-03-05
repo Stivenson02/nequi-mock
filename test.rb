bf29ffdf31d2528eff16908a72d8e96db393dce2
Stivenson02 <stivenson02@gmail.com>
2019-03-04 20:18:12 -0500

    Testing class mysql

--- test.rb
+++ test.rb
@@ -1,19 +1,19 @@
-# Guardaremos toda nuestra configuración en forma de un hash en una variable en ruby
-db_config = {
-    host: 'localhost',
-    adapter: 'postgresql',
-    encoding: 'utf-8',
-    database: 'postgres',
-    schema_search_path: 'public'
-}
-db_config = db_config.merge({username: 'postgres', password: '123456'})
+require_relative 'mysql2.rb'
 
-to_query
+class TestSelect
+  def initialize()
+    @dbconnection = DBConnection.new()
+  end
+  def selectTest
+    results=@dbconnection.query("SELECT* FROM jobs");
+    results.each do |row|
+      puts row["JOB_ID"] # row["id"].is_a? Integer
+    end
+  end
+end
 
-$resutad=(db_config)
-# Primero llamamos a las gemas de postgres y active_record
-# En ruby, para llamar a una gema usamos la palabra require
-require 'pg'
-require 'active_record'
-# Ahora sí utilizamos nuestra conexión
-ActiveRecord::Base.establish_connection(db_config)
+#init
+object = TestSelect.new()
+object.selectTest
+
+
